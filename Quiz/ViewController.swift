4442b167b72921d6c19477c530381d4020b82b0b
jintao <jintao081@qq.com>
2017-10-09 08:54:38 +0800

    使用storyboard 添加UI控件；

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -11,25 +11,50 @@ import SVProgressHUD
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var firstImage: UIImageView!
+    
+    @IBOutlet weak var secondImage: UIImageView!
+    
+    @IBOutlet weak var thirdImage: UIImageView!
+    
+    @IBOutlet weak var FourthImage: UIImageView!
+    
+    @IBOutlet weak var StartBtn: UIButton!
+    
+    @IBOutlet weak var SubmitBtn: UIButton!
+    
+    @IBOutlet weak var RestartBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // 加载数据
         loadData()
+        
+        // 加载UI
+        setupUI()
     }
     
     
-    func loadData()  {
+    private func loadData()  {
 
         SVProgressHUD.show()
         DTQuizManager.shared.loadQuiz(successBlock: { (quizList) in
             SVProgressHUD.dismiss()
+            
             
 
             print(view!)
         }, failureBlock: {_ in
             SVProgressHUD.showError(withStatus: "load error")
         })
+    }
+    
+
+    private func setupUI(){
+        
     }
 
 
